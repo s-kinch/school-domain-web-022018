3358995f3346cf38a770c59a89b7bc3fd2c3024a
sinclairstudy <sinclairstudy@gmail.com>
2018-02-21 18:07:03 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+
+  def sort
+    keys = @roster.keys.sort
+    sorted_hash = {}
+    keys.each do |key|
+      sorted_hash[key] = @roster[key].sort
+    end
+    sorted_hash
+  end
+end
